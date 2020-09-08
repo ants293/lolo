d58a5ffc0d596acf9155e2e00b3e704f47582d39
ants <ants293@gmail.com>
2020-09-08 10:00:38 +0300

    Added /api endpoint. Gets XML from flipboard, passes it to requester.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const requestLib = require('request');
+
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -7,9 +9,24 @@ const buildPath = './client/build';
 
 app.use(express.static(buildPath));
 
+app.post('/api', async (request, response) => {
+    const postFeedResponse = await getPosts('https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss');
+    response.send(postFeedResponse);
+})
+
 app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
+
+function getPosts(url) {
+    return new Promise((resolve, reject) => {
+        requestLib(url, (error, response, body) => {
+            if(error) reject(error)
+
+            else resolve(body)
+        });
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
