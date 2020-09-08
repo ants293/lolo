ae739c687351b57b84e88769dbbd9c40d1cb308f
ants <ants293@gmail.com>
2020-09-08 18:47:23 +0300

    Allow all origins headers.

--- server/index.js
+++ server/index.js
@@ -9,10 +9,16 @@ const buildPath = './client/build';
 
 app.use(express.static(buildPath));
 
-app.post('/api', async (request, response) => {
+app.use((request, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/api', async (request, response) => {
     const postFeedResponse = await getPosts('https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss');
     response.send(postFeedResponse);
-})
+});
 
 app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
