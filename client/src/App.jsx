c65489ca7d94fb1cb91653db2658f332fe221ccd
ants <ants293@gmail.com>
2020-09-09 21:47:15 +0300

    SCSS, Set styles structure.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
 import Articles from "./components/articles/Articles";
 
 export default function App() {
   return (
-    <div className="App">
+    <section>
       <Articles />
-    </div>
+    </section>
   );
 }
