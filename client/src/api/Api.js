613df245444be1281840e9d7501fc9fd163005ba
ants <ants293@gmail.com>
2020-09-10 13:18:47 +0300

    Added request canceling to axios.

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -1,10 +1,19 @@
 import axios from "axios";
+const CancelToken = axios.CancelToken;
+let cancel;
 
 export default class ApiService {
  static hostUrl = process.env.REACT_APP_NODE_SERVICE_URL || window.location.origin;
  static apiUrl = `${this.hostUrl}/api`;
 
  static request(config) {
+
+  if (cancel) {
+   cancel();
+  }
+
+  config.cancelToken = new CancelToken(function(c) { cancel = c})
+
   return axios(config);
  }
 }
