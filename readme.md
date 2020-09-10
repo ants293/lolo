5ccd797936826ffaf2bde7f3f2ac5ec2764ab585
ants <ants293@gmail.com>
2020-09-10 14:36:12 +0300

    Update readme. Minor lint fix in index.

---
+++ readme.md
@@ -0,0 +1,25 @@
+## Development build
+
+In repo root run local server in port 5000:
+
+    npm install
+    npm watch
+    
+For the front-end development build:
+
+    cd /client
+    npm install
+    npm start
+    
+App is available in http://localhost:3000, request by default come from http://localhost:5000
+
+## Production build
+
+    npm install
+    npm start
+    npm run build
+
+## Utilities
+
+    cd /client
+    npm run lint
--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ app.use(express.json());
 
 // Left mostly for test purposes
 app.use((request, response, next) => {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
