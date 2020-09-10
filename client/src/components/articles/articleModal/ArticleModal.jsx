90add3aa75fe40297702938170e29611f733aa1e
ants <ants293@gmail.com>
2020-09-10 09:09:19 +0300

    Modal outside click.

--- client/src/components/articles/articleModal/ArticleModal.jsx
+++ client/src/components/articles/articleModal/ArticleModal.jsx
@@ -2,18 +2,13 @@ import React from 'react';
 import Modal from "../../modal/Modal";
 
 export default function ArticleModal({ isOpen, setOpen, contents }) {
-    console.log("contents", contents);
     return (
         <Modal
             isOpen={isOpen}
+            setOpen={setOpen}
+            modalClasses={'modal--w-850'}
         >
             <div className="generated-content" dangerouslySetInnerHTML={{__html: contents}} />
         </Modal>
-    )
-}
-
-function setContents(contents) {
-    return (
-        contents
     )
 }
--- client/src/components/modal/Modal.jsx
+++ client/src/components/modal/Modal.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import clsx from 'clsx';
 
-export default function Modal({ children, overlayClasses = null, modalClasses = null, isOpen = false }) {
+export default function Modal({ children, overlayClasses = null, modalClasses = null, isOpen = false, setOpen }) {
+    const modalRef = useRef(null);
+    useEffect(() => {
+        document.addEventListener('mousedown', (e) => onOutsideClick(e, modalRef, setOpen));
+
+        return () => {
+            document.removeEventListener('mousedown', onOutsideClick);
+        }
+    })
+
     return (
-        <div className={clsx(['overlay', isOpen && 'overlay--visible', overlayClasses])}>
-            <div className={clsx(['modal', modalClasses])}>
+        <div
+            className={clsx(['overlay', isOpen && 'overlay--visible', overlayClasses])}
+        >
+            <div className={clsx(['modal', modalClasses])} ref={modalRef}>
                 {children}
             </div>
         </div>
     )
+}
+
+function onOutsideClick(e, modalRef, setOpen) {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+        setOpen(false);
+    }
 }
